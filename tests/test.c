f89a1b69733eff8d9969c4960a65d6470fdecb2a
esp_maniac <91507465+espmaniac@users.noreply.github.com>
2022-05-23 21:07:11 +0300

    Update test.c

--- tests/test.c
+++ tests/test.c
@@ -1,4 +1,4 @@
-// arm-linux-gnueabi-gcc -static -nostdlib -marm -march=armv6+nofp test.c
+//arm-none-eabi-gcc -nostartfiles -marm -march=armv6+nofp -marm -march=armv6+nofp ./test.c
 int main(void) {
 	for (int i = 0; i < 10; ++i);
 }
